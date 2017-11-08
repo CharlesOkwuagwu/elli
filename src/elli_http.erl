b5d53711d84ed57964cc09d1f9b4b43c7b8a94f1
Charles <CharlesOkwuagwu@users.noreply.github.com>
2017-11-08 12:14:57 +0100

    Update elli_http.erl

--- src/elli_http.erl
+++ src/elli_http.erl
@@ -30,6 +30,7 @@ start_link(Server, ListenSocket, Options, Callback) ->
 %% transfer. If accept doesn't give us a socket within a configurable
 %% timeout, we loop to allow code upgrades of this module.
 accept(Server, ListenSocket, Options, Callback) ->
+    set_request_timing(),
     case catch elli_tcp:accept(ListenSocket, Server, accept_timeout(Options)) of
         {ok, Socket} ->
             t(accepted),
@@ -45,7 +46,6 @@ accept(Server, ListenSocket, Options, Callback) ->
         {error, Other} ->
             exit({error, Other})
     end.
-
 
 %% @doc: Handle multiple requests on the same connection, ie. "keep
 %% alive".
@@ -67,7 +67,7 @@ keepalive_loop(Socket, NumRequests, Buffer, Options, Callback) ->
 %% socket. Returns the appropriate connection token and any buffer
 %% containing (parts of) the next request.
 handle_request(S, PrevB, Opts, {Mod, Args} = Callback) ->
-    {Method, RawPath, V, B0} = get_request(S, PrevB, Opts, Callback), t(request_start), set_request_timing(),
+    {Method, RawPath, V, B0} = get_request(S, PrevB, Opts, Callback), t(request_start), 
     {RequestHeaders, B1} = get_headers(S, V, B0, Opts, Callback),     t(headers_end),
 
     Req = mk_req(Method, RawPath, RequestHeaders, <<>>, V, S, Callback),
